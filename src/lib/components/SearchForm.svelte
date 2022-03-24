218d506c7c78a7fe2b07504f6444fc3b65c33d1b
yuko makino <yucom2.99@gmail.com>
2022-03-25 08:01:48 +0900

    Formコンポーネントの作成

---
+++ src/lib/components/SearchForm.svelte
@@ -0,0 +1,4 @@
+<input type="search">
+<button>検索</button>
+<input type="checkbox">歌詞検索
+<input type="checkbox">YouTube
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import SearchForm from '../lib/components/SearchForm.svelte'
+</script>
+
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <SearchForm/>
+</main>
