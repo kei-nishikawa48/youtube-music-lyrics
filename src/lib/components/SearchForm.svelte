efccbe6e974a41f75b03b6d47179598d3bf336f9
yuko makino <yucom2.99@gmail.com>
2022-03-28 11:46:11 +0900

    バインディングの追加

--- src/lib/components/SearchForm.svelte
+++ src/lib/components/SearchForm.svelte
@@ -1,4 +1,10 @@
-<input type="search">
+<script>
+    export let value = '';
+    let searchYouTube = false;
+    let searchLyrics = false;
+</script>
+
+<input type="text" bind:value placeholder="Search...">
 <button>検索</button>
-<input type="checkbox">歌詞検索
-<input type="checkbox">YouTube
+<input type="checkbox" bind:checked={searchYouTube} />YouTube
+<input type="checkbox" bind:checked={searchLyrics} />歌詞検索
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,16 @@
 <script>
     import SearchForm from '../lib/components/SearchForm.svelte'
+
+    let searchWord = '';
+
+    const handleSubmit = () => {
+        console.log(searchWord)
+    }
+
 </script>
 
-<main>
-    <h1>Welcome to SvelteKit</h1>
-    <SearchForm/>
-</main>
+<h1>Welcome to SvelteKit</h1>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <SearchForm bind:value={searchWord} />
+</form>
