20c4403cc35557d894ecd3da256448e8f7aa78db
yuko makino <yucom2.99@gmail.com>
2022-04-01 14:30:53 +0900

    CheckBoxコンポーネントの追加

--- src/lib/components/SearchForm.svelte
+++ src/lib/components/SearchForm.svelte
@@ -1,10 +1,6 @@
 <script>
     export let value = '';
-    let searchYouTube = false;
-    let searchLyrics = false;
 </script>
 
 <input type="text" bind:value placeholder="Search...">
 <button>検索</button>
-<input type="checkbox" bind:checked={searchYouTube} />YouTube
-<input type="checkbox" bind:checked={searchLyrics} />歌詞検索
