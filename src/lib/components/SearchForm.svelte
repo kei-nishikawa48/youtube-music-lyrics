33bdf49cd0a2ddff21ef752138db9bd4d5a6960b
yuko makino <yucom2.99@gmail.com>
2022-06-22 12:57:14 +0900

    nodeのバージョン変更とTailwindcssの適用

--- src/lib/components/SearchForm.svelte
+++ src/lib/components/SearchForm.svelte
@@ -1,18 +1,23 @@
 <script>
-    export let value = '';
-    export let searchYouTube = false;
-    export let searchLyrics = false;
+	export let value = '';
+	export let searchYouTube = false;
+	export let searchLyrics = false;
 
-    const handleSubmit = () => {
-        console.log(value)
-        console.log(searchYouTube)
-        console.log(searchLyrics)
-    }
+	const handleSubmit = () => {
+		console.log(value);
+		console.log(searchYouTube);
+		console.log(searchLyrics);
+	};
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-    <input type="text" bind:value placeholder="Search...">
-    <button>検索</button>
-    <input type="checkbox" bind:checked={searchYouTube} />YouTube
-    <input type="checkbox" bind:checked={searchLyrics} />歌詞検索
+<form on:submit|preventDefault={handleSubmit} class="w-full flex justify-center items-baseline m-2">
+	<input
+		type="text"
+		class="text-gray-700 bg-white border border-solid border-gray-300 rounded mr-2"
+		bind:value
+		placeholder="Search..."
+	/>
+	<button class="px-5 py-1.5 bg-blue-600 text-white text-xs rounded mr-2">検索</button>
+	<input type="checkbox" bind:checked={searchYouTube} />YouTube
+	<input type="checkbox" bind:checked={searchLyrics} />歌詞検索
 </form>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,14 @@
 <script>
-  import "../styles/tailwind.css";
-  import SearchForm from "$lib/components/SearchForm.svelte";
+	import '../styles/tailwind.css';
+	import SearchForm from '$lib/components/SearchForm.svelte';
 </script>
 
 <main>
-  <div class="search-form flex justify-center">
-    <SearchForm />
-  </div>
-  
-  <div class="search-results flex flex-row">
-    <slot />
-  </div>
-</main>
+	<div class="search-form w-full">
+		<SearchForm />
+	</div>
 
+	<div class="search-results flex flex-row">
+		<slot />
+	</div>
+</main>
