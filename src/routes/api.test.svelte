afed6edd41420f102fb085ccfddf5cae3178d4f7
mae616 <50073576+mae616@users.noreply.github.com>
2022-09-17 00:02:38 +0900

    test

--- src/routes/api.test.svelte
+++ src/routes/api.test.svelte
@@ -24,9 +24,9 @@
 	async function handleLyricsListSearch(): Promise<void> {
 		if (inputValue) {
 			const res = await fetch(`./api/lyricsListSearch-${inputValue}`);
-			const data = await res.json();
+			const data = await res.text();
 			console.log(data);
-			lyrics_url = data.result[0].lyrics_url;
+			// lyrics_url = data.result[0].lyrics_url;
 		} else {
 			alert('inputText is empty');
 		}
