de85e4be483c35c71973f8384b38e9778cbf8f75
mae616 <50073576+mae616@users.noreply.github.com>
2022-09-16 23:55:18 +0900

    test

--- src/routes/api/lyricsListSearch-[query].ts
+++ src/routes/api/lyricsListSearch-[query].ts
@@ -4,17 +4,18 @@ export async function get({ params }) {
 	const response = await fetch(genius_search_url);
 	const response_json = await response.json();
 
-	console.log(response_json);
+	return { body: { status: '200', resoult: response_json } };
+	// console.log(response, response_json);
 
-	const result = response_json.response.hits.map((el) => ({
-		artist: el.result.artist_names,
-		artist_id: el.result.primary_artist.id,
-		title: el.result.title,
-		lyrics_url: el.result.url,
-		song_art_image_url: el.result.song_art_image_url
-	}));
+	// const result = response_json.response.hits.map((el) => ({
+	// 	artist: el.result.artist_names,
+	// 	artist_id: el.result.primary_artist.id,
+	// 	title: el.result.title,
+	// 	lyrics_url: el.result.url,
+	// 	song_art_image_url: el.result.song_art_image_url
+	// }));
 
-	return {
-		body: { status: '200', result: result }
-	};
+	// return {
+	// 	body: { status: '200', result: result }
+	// };
 }
