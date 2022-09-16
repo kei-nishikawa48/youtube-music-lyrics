2a8234f4375bfdab1497fe1efa6f006809f624d3
mae616 <50073576+mae616@users.noreply.github.com>
2022-09-16 23:41:46 +0900

    install cheerio

--- src/routes/api/lyricsSearch.ts
+++ src/routes/api/lyricsSearch.ts
@@ -1,4 +1,4 @@
-import { JSDOM } from 'jsdom';
+import { load } from 'cheerio';
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 /** 歌詞取得
@@ -11,13 +11,19 @@ export async function post({ request }) {
 	const lyrics_response = await fetch(lyrics_url);
 
 	let lyricsHTML = '';
-	const jsdom = new JSDOM(await lyrics_response.text());
-	const divAll = jsdom.window.document.querySelectorAll('div');
-	divAll.forEach((divElement: HTMLElement) => {
-		if (divElement.dataset.lyricsContainer === 'true') {
-			lyricsHTML += divElement.innerHTML;
-		}
-	});
+	const $ = load(await lyrics_response.text());
+	const isLyricsClass = $('div[class="lyrics"]').text().trim();
+	if (!isLyricsClass) {
+		$('div[class^="Lyrics__Container"]').each((i, elem) => {
+			if ($(elem).text().length !== 0) {
+				const snippet = $(elem)
+					.html()
+					.replace(/<br>/g, '\n')
+					.replace(/<(?!\s*br\s*\/?)[^>]+>/gi, '');
+				lyricsHTML += $('<textarea/>').html(snippet).text().trim() + '\n\n';
+			}
+		});
+	}
 
 	return {
 		body: { status: '200', lyricsHTML }
